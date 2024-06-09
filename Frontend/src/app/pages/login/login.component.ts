0bdafee587b268e5f38d6cf57cb8e9473ebf80ac
ffabiolad <ffabioladelgadormz@gmail.com>
2024-06-09 01:03:00 -0600

    rutas

--- Frontend/src/app/pages/login/login.component.ts
+++ Frontend/src/app/pages/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from '../../shared/loading/loading.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [CommonModule, LoadingComponent, RouterModule] 
 })
 export class LoginComponent {}
