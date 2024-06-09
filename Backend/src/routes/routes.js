06fc7774252c972f8373351dde494bae326839da
Gerardo Femat Delgado <git@femat.dev>
2024-06-09 15:22:15 -0600

    Comercio

--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -3,7 +3,9 @@ const routerApi = express.Router();
 const router = express.Router();
 
 import campus from './campus.js';
+import comercio from './comercio.js';
 
+router.use('/comercio', comercio);
 router.use('/campus', campus);
 
 routerApi.use('/api', router);
